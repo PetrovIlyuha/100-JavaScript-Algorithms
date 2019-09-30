67862363214dd23763fb733aad02087cbb9a51b5
Ilya Petrov <dnamix1@gmail.com>
2019-09-30 13:59:02 +0300

    added solution to array's consequtive pairs of elements alternate summation and multiplication reducing to one number problem

---
+++ arrayConversion.js
@@ -0,0 +1,25 @@
+function arrayConversion(inputArray) {
+  let isOdd = true;
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd)
+    isOdd = !isOdd;
+  }
+
+  return inputArray[0];
+}
+
+function sumProduct(nums, isOdd) {
+  const sumProducts = [];
+  if (isOdd) {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] + nums[i+1]);
+    }
+  } else {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] * nums[i+1]);
+    }
+  }
+  return sumProducts;
+}
+
+console.log(arrayConversion([1,2,3,4,5,6,7,8]));
