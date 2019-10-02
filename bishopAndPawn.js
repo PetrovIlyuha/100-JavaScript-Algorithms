46ce55b1979066f4a5dfd776feb963e6956bd1b4
Ilya Petrov <dnamix1@gmail.com>
2019-10-02 09:24:12 +0300

    added naive solution to the problem of `bishop and pawn`

---
+++ bishopAndPawn.js
@@ -0,0 +1,25 @@
+function bishopAndPawn(bishop, pawn) {
+  const board = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8
+  };
+
+  const bishopX = board[bishop[0]];
+  const bishopY = parseInt(bishop[1]);
+  const pawnX = board[pawn[0]];
+  const pawnY = parseInt(pawn[1]);
+
+  if (bishopX + bishopY === pawnX + pawnY || bishopX + pawnY === pawnX + bishopY) {
+    return true;
+  }
+  return false;
+}
+
+console.log(bishopAndPawn('a1', 'c3'));
+
