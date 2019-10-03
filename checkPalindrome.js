91f591562a4941e907d8239da9b2a1249f4abfa9
Ilya Petrov <dnamix1@gmail.com>
2019-10-03 09:22:23 +0300

    added solution to `check the string for being a palindrome`

---
+++ checkPalindrome.js
@@ -0,0 +1,9 @@
+function checkPalindrome(inputString) {
+  const originalLowerCase = inputString.toLowerCase();
+
+  return originalLowerCase.split('').reverse().join('') === originalLowerCase ? 'Valid Palindrome' : 'Not a palindrome';
+}
+
+console.log(checkPalindrome('digid'))
+console.log(checkPalindrome('digital'))
+console.log(checkPalindrome('kayak'))
