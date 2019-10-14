2f89f4f2faaa0f0690317ad526bed724e3fa11cb
Ilya Petrov <dnamix1@gmail.com>
2019-10-14 16:43:43 +0300

    slightly changed palindrome checker solution

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -4,6 +4,6 @@ function checkPalindrome(inputString) {
   return originalLowerCase.split('').reverse().join('') === originalLowerCase ? 'Valid Palindrome' : 'Not a palindrome';
 }
 
-console.log(checkPalindrome('digid'))
+console.log(checkPalindrome('digid digid digid'))
 console.log(checkPalindrome('digital'))
 console.log(checkPalindrome('kayak'))
