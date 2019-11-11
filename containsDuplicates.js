2a8b865fd7d30a3b5e161c0d8e2049b84d60be7c
Ilya Petrov <dnamix1@gmail.com>
2019-11-11 14:05:00 +0300

    check for presence of duplicates in an array

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -1,16 +1,13 @@
 function containsDuplicates(arr) {
-  let sortedArr = arr.sort((a,b) => a - b);
+  let sortedArr = arr.sort((a, b) => a - b);
 
-  for (let i = 0; i < sortedArr.length; i++){
-    if (sortedArr[i] === sortedArr[i+1]){
-      return true
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (sortedArr[i] === sortedArr[i + 1]) {
+      return true;
     }
   }
   return false;
 }
 
-
-console.log(containsDuplicates([1,3,5,7,5]));
-console.log(containsDuplicates([1,3,2,7,5]));
-
-
+console.log(containsDuplicates([1, 3, 5, 7, 5]));
+console.log(containsDuplicates([1, 3, 2, 7, 5]));
