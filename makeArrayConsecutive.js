b78f1d74d5c505d6d167b32eb6f8e4278cd9313f
Ilya Petrov <dnamix1@gmail.com>
2020-01-25 06:52:04 +0300

    make array consecutive task modified

--- makeArrayConsecutive.js
+++ makeArrayConsecutive.js
@@ -3,6 +3,11 @@ function makeArrayConsecutive(arr) {
   const sortedNumbers = arr.sort((a, b) => a - b);
   const min = sortedNumbers[0];
   const max = sortedNumbers[sortedNumbers.length - 1];
+  for (let i = min; i < max; i++) {
+    if (arr.indexOf(i) === -1) {
+      count++;
+    }
+  }
   return count;
 }
 
