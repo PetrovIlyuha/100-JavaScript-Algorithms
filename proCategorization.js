c19f4163f105121ae826ab05da74fcee381d67e1
Ilya Petrov <dnamix1@gmail.com>
2020-01-27 08:04:25 +0300

    professionals categorization task

---
+++ proCategorization.js
@@ -0,0 +1,41 @@
+function proCategorization(professinals, preferences) {
+  let prosPreferences = {};
+  let proPrefs = [];
+  for (let i = 0; i < professinals.length; i++) {
+    for (let j = 0; j < preferences[i].length; j++) {
+      if (prosPreferences.hasOwnProperty(preferences[i][j])) {
+        prosPreferences[preferences[i][j]].push(professinals[i]);
+      } else {
+        prosPreferences[preferences[i][j]] = [professinals[i]];
+      }
+    }
+  }
+  for (const prop in prosPreferences) {
+    proPrefs.push([[prop], [...prosPreferences[prop]]]);
+  }
+
+  proPrefs = proPrefs.sort((pref1, pref2) => {
+    const pref1Lower = pref1[0][0].toLowerCase();
+    const pref2Lower = pref2[0][0].toLowerCase();
+    if (pref1Lower > pref2Lower) {
+      return 1;
+    }
+    if (pref1Lower < pref2Lower) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return proPrefs;
+}
+
+console.log(
+  proCategorization(
+    ["Ivan", "Sergey", "Maria"],
+    [
+      ["Dental Care", "Financial Consultancy", "Web Design"],
+      ["Financial Consultancy", "Electronics Repair"],
+      ["Electronics Repair", "Web Design"]
+    ]
+  )
+);
